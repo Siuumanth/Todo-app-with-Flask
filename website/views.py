cf6750b71b705557e4a27111da5aea0cc8b46161
Siuumanth <gsumanth3005@gmail.com>
2024-12-06 00:13:46 +0530

    Addedd some stuff

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
 # for storing the api routes
 # we create a container for our webpages related to the actual app
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
-    return '<h1>Welcome<h1>'  # the browser will render this
+    return render_template("home.html")  # the browser will render this
 
